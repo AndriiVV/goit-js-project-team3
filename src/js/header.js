eeec29705ba4b56a7fd53b0e23a139b642da66bb
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-01-21 21:37:49 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,56 +1,60 @@
-import countries from "../json/countries.json";
+import countries from '../json/countries.json';
 const axios = require('axios');
 
 const refs = {
-  form: document.querySelector("#search-form"),
-  inputSearch: document.querySelector("[name=searchQuery]"),
-  inputCountry: document.querySelector("[name=searchCountryQuery]"),
-  cardEL:document.querySelector(".search__list")
-}
+  form: document.querySelector('#search-form'),
+  inputSearch: document.querySelector('[name=searchQuery]'),
+  inputCountry: document.querySelector('[name=searchCountryQuery]'),
+  cardEL: document.querySelector('.search__list'),
+};
 console.log(refs.cardEL);
-let code = "";
-let searchId = "";
+let code = '';
+let searchId = '';
 
 function renderSearchCountry(countries) {
-  const markup = countries.map(item => {
-    code = item.alpha;
-    return  `<li 
+  const markup = countries
+    .map(item => {
+      code = item.alpha;
+      return `<li 
     class="country-item"> <span class="text-item"> ${item.name} / (${item.alpha})</span> </li>
-    `}).join("")
-  
+    `;
+    })
+    .join('');
 }
-renderSearchCountry(countries)
+renderSearchCountry(countries);
 
-refs.form.addEventListener("submit", onSubmitForm)
+refs.form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(event) {
-  event.preventDefault()
+  event.preventDefault();
   searchId = event.target.searchQuery.value;
-  code = event.target.searchCountryQuery.value
-  fetchUrl(searchId,code)
-  event.target.reset()
-  
+  code = event.target.searchCountryQuery.value;
+  fetchUrl(searchId, code);
+  event.target.reset();
 }
 
-async function fetchUrl(kewyord,code) {
-   try {
-    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${kewyord}&countryCode=${code}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`);
-     const data = await response.json();
-     renderCard(data._embedded.events)
+async function fetchUrl(kewyord, code) {
+  try {
+    const response = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${kewyord}&countryCode=${code}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`,
+    );
+    const data = await response.json();
+    renderCard(data._embedded.events);
     console.log(data._embedded);
   } catch (error) {
     error.message;
   }
-};
+}
 
 function renderCard(info) {
-  const cardMarkup = info.map(item => {
-    // console.log(item);
-    // console.log(item.name);
-    // console.log(item.dates.start.localDate);
-    // console.log(item._embedded.venues[0].name);
-    // console.log(item.images[1].url);
-   return `
+  const cardMarkup = info
+    .map(item => {
+      // console.log(item);
+      // console.log(item.name);
+      // console.log(item.dates.start.localDate);
+      // console.log(item._embedded.venues[0].name);
+      // console.log(item.images[1].url);
+      return `
     <div class="card-wrap">
       <img width="180" height="227" src="${item.images[1].url}" alt="${item.name}">
       <ul class="search__item">
@@ -63,10 +67,15 @@ function renderCard(info) {
           <span class="card-text-place">${item._embedded.venues[0].name}</span>
         </li>
       </ul>
-    </div>`
-  }).join("")
+    </div>`;
+    })
+    .join('');
 
-  refs.cardEL.innerHTML = cardMarkup
+  refs.cardEL.innerHTML = cardMarkup;
 }
 
-fetch(`https://app.ticketmaster.com/discovery/v2/events/G5v0Zpsu6Edy5/images.json?apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`).then(res => res.json()).then(data => console.log(data))
+fetch(
+  `https://app.ticketmaster.com/discovery/v2/events/G5v0Zpsu6Edy5/images.json?apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`,
+)
+  .then(res => res.json())
+  .then(data => console.log(data));
