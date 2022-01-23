4c98ae8526290c5230fa396d902b7ec20d44c219
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2022-01-24 00:39:48 +0200

    модалка
    зробив модалку з командою
    та добавив position absolute в модалку основну

--- src/js/about.js
+++ src/js/about.js
@@ -1,15 +1,13 @@
 const refs = {
-    openModalBtn: document.querySelector('[data-about-open]'),
-    closeModalBtn: document.querySelector('[data-about-close]'),
-    modal: document.querySelector('[data-about]'),
+  openModalBtn: document.querySelector('[data-about-open]'),
+  closeModalBtn: document.querySelector('[data-about-close]'),
+  modal: document.querySelector('[data-about]'),
 };
 
-// refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('keydown', toggleModal);
+
 refs.openModalBtn.addEventListener('click', toggleModal);
-// console.log('hello');
 
 function toggleModal() {
-    refs.modal.classList.toggle('visually-hidden');
+  refs.modal.classList.toggle('visually-hidden');
 }
