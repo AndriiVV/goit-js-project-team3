8a5a9e4d75daaee43870b3f9b7fa9ee5a1680871
Valentin787 <89661234+Valentin787@users.noreply.github.com>
2022-01-24 19:08:40 +0200

    fetch-info-card

---
+++ src/js/fetchEvent.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+function fetchEvent(id) {
+ 
+  return axios.get(`https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`)
+    .then(res => {
+      // console.log(res.data);
+      // console.log(res.data.info)
+      // console.log(res.data.dates.start.localDate)
+      // console.log(res.data.dates.start.localTime)
+      // console.log(res.data._embedded.venues[0].name)
+      // console.log(res.data.dates.timezone)
+      // console.log(res.data.name)
+      // console.log(res.data.priceRanges[0].min)
+      // console.log(res.data.priceRanges[0].max)
+      // console.log(res.data.priceRanges[0].currency)
+      // console.log(res.data.products[0].url)
+      // console.log(res.data.images[1].url)
+    })
+    .catch(error => error)
+  
+}
+export default fetchEvent
