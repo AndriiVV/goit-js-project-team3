b29eb2e518c323b8a77f61194bcbf0adc8f341cd
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-01-25 01:14:30 +0200

    bugfix in pagination

--- src/js/clickNumberPage.js
+++ src/js/clickNumberPage.js
@@ -1,26 +1,22 @@
-import renderPage from "./render-page";
-import renderCard from "./render-card"
-import fetchNumberClick from './fetchNumberPage'
+import renderPage from './render-page';
+import renderCard from './render-card';
+import fetchNumberClick from './fetchNumberPage';
 
 let numberPage = 0;
 
-
-const mainUl = document.querySelector(".main__pagination")
-mainUl.addEventListener("click", onClickPage)
+const mainUl = document.querySelector('.main__pagination');
+mainUl.addEventListener('click', onClickPage);
 
 function onClickPage(event) {
-  event.preventDefault()
+  event.preventDefault();
   console.log(event.target.tagName);
-  if (event.target.tagName !== "A") {
-    return false
+  if (event.target.tagName !== 'A') {
+    return false;
   } else {
-    const finalHref = event.target.dataset.href
-    numberPage = `&page=${event.target.textContent}&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`;
+    const finalHref = event.target.dataset.href;
+    numberPage = `&page=${+event.target.textContent - 1}&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`;
 
-    fetchNumberClick(finalHref, numberPage, event.target)
+    fetchNumberClick(finalHref, numberPage, event.target);
   }
 }
-export default onClickPage
-
-
-
+export default onClickPage;
