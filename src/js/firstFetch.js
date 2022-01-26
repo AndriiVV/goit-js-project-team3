491687845ac8dc68ffbb2ac73c2cb8d690506850
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-01-26 09:46:12 +0200

    Bugfix in pagination

--- src/js/firstFetch.js
+++ src/js/firstFetch.js
@@ -1,5 +1,5 @@
-import renderCard from "./render-card"
-import renderPage from "./render-page";
+import renderCard from './render-card';
+import renderPage from './render-page';
 import Notiflix from 'notiflix';
 
 console.log(window.screen.width);
@@ -7,30 +7,31 @@ async function startPage(idCountry, countryName) {
   try {
     if (window.screen.width >= 768 && window.screen.width <= 1278) {
       const res = await fetch(
-        `https://app.ticketmaster.com/discovery/v2/events.json?size=21&countryCode=${idCountry}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`
-      )
-      const finalEl = await res.json()
-      renderCardForTablet(finalEl,countryName)
-    
-    }
-    else {
+        `https://app.ticketmaster.com/discovery/v2/events.json?size=21&countryCode=${idCountry}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`,
+      );
+      const finalEl = await res.json();
+      renderCardForTablet(finalEl, countryName);
+    } else {
       const res = await fetch(
-        `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${idCountry}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`
-      )
-      const finalEl = await res.json()
-      renderCardForTablet(finalEl,countryName)
+        `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${idCountry}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`,
+      );
+      const finalEl = await res.json();
+      renderCardForTablet(finalEl, countryName);
     }
   } catch (error) {
-    error.message
+    error.message;
   }
 }
 
 function renderCardForTablet(info, name) {
-  
   renderCard(info._embedded.events);
-  renderPage(info.page.totalPages, info._links.self.href,)
-  setTimeout(() => { Notiflix.Notify.success(`HELLO, You see EVENTS only ${name}`) }, 300)
-  setTimeout(() => { Notiflix.Notify.info('👈 BUT, You Can write or Say KEYWORD, and see events only you want') }, 4000)
+  renderPage(Math.min(47, info.page.totalPages), info._links.self.href);
+  setTimeout(() => {
+    Notiflix.Notify.success(`HELLO, You see EVENTS only ${name}`);
+  }, 300);
+  setTimeout(() => {
+    Notiflix.Notify.info('👈 BUT, You Can write or Say KEYWORD, and see events only you want');
+  }, 4000);
 }
 
-export default startPage
+export default startPage;
