9d49b2ed0306989a6b8f581ec50678927f784a6b
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-01-26 15:05:14 +0200

    Bugfix/remove console.log

--- src/js/fetchAuthor.js
+++ src/js/fetchAuthor.js
@@ -1,19 +1,17 @@
-import renderCard from "./render-card"
-import renderPage from"./render-page"
+import renderCard from './render-card';
+import renderPage from './render-page';
 async function fetchByAuthor(nameEvent) {
-
   try {
-    const responce = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${nameEvent}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`)
+    const responce = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/events.json?keyword=${nameEvent}&source=Ticketmaster&apikey=841T1YMOPnVKBAAycVhND0Lj4GpnyoMw`,
+    );
     const infoAuthor = await responce.json();
-    console.log(infoAuthor);
-    renderCard(infoAuthor._embedded.events)
-    renderPage(Math.min(47, infoAuthor.page.totalPages), infoAuthor._links.self.href)
-    
-    
+    // console.log(infoAuthor);
+    renderCard(infoAuthor._embedded.events);
+    renderPage(Math.min(47, infoAuthor.page.totalPages), infoAuthor._links.self.href);
   } catch (error) {
-    error.message
+    error.message;
   }
-  
 }
 
 export default fetchByAuthor;
